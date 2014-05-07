857f02acc74c8dc7df53d9c2a648420890ab7e7c
Teng <otec.teng@gmail.com>
2014-05-07 17:57:39 +0800

    Update rbenv.sh

--- rbenv.sh
+++ rbenv.sh
@@ -1,6 +1,6 @@
 #/etc/profile.d/rbenv.sh
 if [[ ! -d "${HOME}/.rbenv" ]]; then
-  export RBENV_ROOT=/usr/local/rbenv'
-  export PATH="$RBENV_ROOT/bin:$PATH"'
-  eval "$(rbenv init -)"'
+  export RBENV_ROOT=/usr/local/rbenv
+  export PATH="$RBENV_ROOT/bin:$PATH"
+  eval "$(rbenv init -)"
 fi
