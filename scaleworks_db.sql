5a6f0019294cfb17b682336e16851aedf0ca6e50
Teng <otec.teng@gmail.com>
2014-05-09 17:30:58 +0800

    Update scaleworks_db.sql

--- scaleworks_db.sql
+++ scaleworks_db.sql
@@ -1,4 +1,4 @@
 create database scaleworks_production;
-create user 'scaleworks'@'%' identified by 'scaleworks'
-grant all privileges on scaleworks_production.* to 'scaleworks'@'%' with grant option;
+create user 'scaleworks'@'localhost' identified by 'scaleworks';
+grant all privileges on scaleworks_production.* to 'scaleworks'@'localhost' with grant option;
 flush privileges;
