6c3763e99792520e9f7f17a0b9f81246d405063a
Teng <otec.teng@gmail.com>
2014-09-15 10:28:14 +0800

    Update scaleworks_db.sql

--- scaleworks_db.sql
+++ scaleworks_db.sql
@@ -1,4 +1,4 @@
-create database scaleworks_production;
+create database scaleworks_production character set utf8;
 create user 'scaleworks'@'localhost' identified by 'scaleworks';
 grant all privileges on scaleworks_production.* to 'scaleworks'@'localhost' with grant option;
 flush privileges;
